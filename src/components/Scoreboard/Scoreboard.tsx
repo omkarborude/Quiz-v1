eff5cb139fe6b027e1407f3e8cbd557276e6f90c
omkar <borudeomkar41@gmail.com>
2021-06-17 19:45:10 +0530

    Feat:score

--- src/components/Scoreboard/Scoreboard.tsx
+++ src/components/Scoreboard/Scoreboard.tsx
@@ -1,18 +1,36 @@
-import { useQuiz } from "../../QuizContext/QuizContext";
-import { Result } from "../../QuizContext/quizReducer";
-import "./score.css";
 import { ReviewAnswer } from "../ReviewAnswer/ReviewAnswer";
+import { useQuiz } from "../../QuizContext/QuizContext";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAuth } from "../index";
+import axios from "axios";
+import "./score.css";
 
 export const Scoreboard = () => {
   const { quizState } = useQuiz();
+  const { loginData, isUserLogin } = useAuth();
+  const navigate = useNavigate();
 
-  const getRightAnswers = (resultArray: Result[]): number => {
-    const rightAnswers = resultArray.filter(
-      (result) => result.correctOption === result.selectedOption
-    );
-    console.log(rightAnswers.length);
-
-    return rightAnswers.length;
+  const updateScore = async () => {
+    try {
+      toast("Saving Score !", {
+        position: "top-right",
+        autoClose: 2000,
+      });
+      const res = await axios.post(
+        `https://neogquizbackend.omkarborude8354.repl.co/score/savescore/${loginData?.userId}`,
+        {
+          username: loginData?.username,
+          quiz: quizState.currentQuiz?.topic,
+          score: quizState.score,
+        }
+      );
+      if (res.status === 200) {
+        navigate("/");
+      }
+    } catch (error) {
+      alert(error);
+    }
   };
 
   return (
@@ -22,7 +40,18 @@ export const Scoreboard = () => {
           <div className="scoreboard_card">
             <h3>Your ScoreBoard </h3>
             <p>Your score is : {quizState.score}</p>
+            <div className="scorecard-submit-btn-div">
+              <button
+                className="btn-submit-to-server"
+                onClick={() => {
+                  isUserLogin ? updateScore() : navigate("/login");
+                }}
+              >
+                Save Score <i className="fas fa-cloud-upload-alt"></i>
+              </button>
+            </div>
           </div>
+
           <div className="answered-review-div">
             <ReviewAnswer />
           </div>
