b03cb8d753567b183c3bcbaf9936495893c9b284
omkar <borudeomkar41@gmail.com>
2021-06-17 19:43:19 +0530

    Feat:Dash

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import { useQuiz } from "../../QuizContext/QuizContext";
+import { Link } from "react-router-dom";
 import "./dash.css";
 
 export const Dashboard = () => {
@@ -13,6 +13,7 @@ export const Dashboard = () => {
       type: "INITIALIZE_QUESTION_NUMBER_AND_SCORE",
     });
   };
+
   return (
     <div>
       <div className="quiz_card-main-div">
@@ -26,11 +27,11 @@ export const Dashboard = () => {
                 <div className="quiz_tag">{quiz.topic}</div>
                 <div className="quiz_desc">{quiz.description}</div>
                 <div className="quiz_btn-play">
-                  <Link to={`/quiz/${quiz.id}`} key={quiz.id}>
+                  <Link to={`/quiz/${quiz._id}`} key={quiz._id}>
                     <button
                       className="btn-play-now"
-                      id={quiz.id}
-                      onClick={() => takeQuiz(quiz.id)}
+                      id={quiz._id}
+                      onClick={() => takeQuiz(quiz._id)}
                     >
                       Play Now
                     </button>
@@ -41,6 +42,30 @@ export const Dashboard = () => {
           );
         })}
       </div>
+      <footer className="home-footer-div">
+        <b>Connect With Me : </b>
+        <a
+          href="https://github.com/omkarborude"
+          target="_blank"
+          style={{ color: "inherit" }}
+        >
+          <i className="fab fa-github"></i>
+        </a>
+        <a
+          href="https://twitter.com/omkar_Borude_"
+          target="_blank"
+          style={{ color: "inherit" }}
+        >
+          <i className="fab fa-twitter-square"></i>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/omkar-borude-b4583016b/"
+          target="_blank"
+          style={{ color: "inherit" }}
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+      </footer>
     </div>
   );
 };
